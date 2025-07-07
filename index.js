f03a2df9246c1a2f3355c3c8102788f9975e65ad
nour kouider <nourkouider05@gmail.com>
2025-07-07 15:49:33 +0100

    docker-compose changes

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const itemsRoute = require("./routes/items");
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://nourkouider05:nour0205@cluster0.pbcypam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use(express.json());
+app.use("/api/items", itemsRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello DevOps World!");
---
+++ routes/items.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+const Item = require("../models/Item");
+
+// Create new item
+router.post("/", async (req, res) => {
+  const item = new Item({
+    name: req.body.name,
+  });
+  try {
+    const savedItem = await item.save();
+    res.json(savedItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Get all items
+router.get("/", async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get single item
+router.get("/:id", async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) return res.status(404).json({ message: "Item not found" });
+    res.json(item);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Update item
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedItem = await Item.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { new: true }
+    );
+    if (!updatedItem)
+      return res.status(404).json({ message: "Item not found" });
+    res.json(updatedItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete item
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedItem = await Item.findByIdAndDelete(req.params.id);
+    if (!deletedItem)
+      return res.status(404).json({ message: "Item not found" });
+    res.json({ message: "Item deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
